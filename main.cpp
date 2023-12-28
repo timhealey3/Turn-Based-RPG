eac7a551daeed7a52a4892a782c05e35f1c30756
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2023-12-27 22:54:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
-#include "allclasses.h"
-
+#include "playercharacter.h"
 int main() {
 
-    Warrior warr1;
-    Wizard wiz1;
-    Cleric cler1;
-    Hobbit hob1;
-    
-    return 0;
+	// just test code for now, replace 'Warrior' with your class to test
+	PlayerCharacter p1(new Cleric());
+	for (int i = 0; i < 2; i++) {
+		std::cout
+			<< p1.getClassName()
+			<< " Level " << p1.getLevel() << '\n'
+			<< "-EXP: " << p1.getCurrentEXP() << '/' << p1.getEXPToNextLevel() << '\n'
+			<< "-HP: " << p1.getCurrentHP() << '/' << p1.getMaxHP() << '\n'
+			<< "-MP: " << p1.getCurrentMP() << '/' << p1.getMaxMP() << '\n'
+			<< "-Stength: " << p1.getTotalStrength() << '\n'
+			<< "-Intellect: " << p1.getTotalIntellect() << '\n'
+			<< "-Agility: " << p1.getTotalAgility() << '\n'
+			<< "-Armor: " << p1.getTotalArmor() << '\n'
+			<< "-Resist: " << p1.getTotalElementRes() << '\n';
+
+		auto AllAbilities= p1.getAbilityList();
+
+		std::cout << "-Abilities:\n";
+		for (auto& abil : AllAbilities) {
+			std::cout << "  -" << abil.Name << '\n';
+		}
+
+		if (i < 1) 
+		{
+			p1.gainEXP(100u);
+			Buff arm_buff("StoneShield", 10, 0, 0, 0, 5);
+			p1.ApplyBuff(arm_buff);
+		}
+	}
+
+	return 0;
 }
