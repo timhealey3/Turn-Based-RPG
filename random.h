b7cf8bb1a106b3a901a9534337a9ea7a863facb5
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2024-01-02 23:45:30 -0500

    Create random.h

---
+++ random.h
@@ -0,0 +1,23 @@
+#include "random.h"
+#include <chrono>
+#include <random>
+
+namespace Random {
+    [[nodiscard]] uint32_t NTKR(uint32_t n, uint32_t k) noexcept
+    {
+        static uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+        static std::seed_seq ss{ uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed >> 32) };
+        static std::mt19937 mgen(ss);
+
+        // check if user put n <= k to process bounds
+        if (n <= k) {
+            std::uniform_int_distribution<std::mt19937::result_type> ntkd(n, k);
+            return ntkd(mgen);
+        }
+        else // else n > k, still functions as bounds k to n
+        {
+            std::uniform_int_distribution<std::mt19937::result_type> ntkd(k, n);
+            return ntkd(mgen);
+        }
+    }
+}
